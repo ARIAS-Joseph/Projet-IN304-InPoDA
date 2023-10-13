082ff0a4d7f45d589d7d55291bc4c24035d26cfd
Joseph ARIAS <arias.joseph.biot@gmail.com>
2023-10-13 09:44:57 +0200

    Amélioration de la fonction extract pour aussi extraire les utilisateurs mentionnés

--- Projet IN304 class.py
+++ Projet IN304 class.py
@@ -11,10 +11,12 @@ class Tweet:
 
     nb_tweets = 0
     used_hashtag = {}
+    user_mentioned = {}
 
     def __init__(self, tweet=dict):
         Tweet.nb_tweets += 1
         tweet['Hashtags'] = []
+        tweet['Mentions'] = []
         self.id = tweet['id']
         self.localisation = tweet['AuthorLocation']
         self.date = tweet['CreatedAt']
@@ -22,37 +24,46 @@ class Tweet:
         self.langue = tweet['TweetLanguage']
         self.texte = tweet['TweetText']
         self.hashtag = tweet['Hashtags']
-        self.extact_hashtag()
+        self.mention = tweet['Mentions']
+        self.extract_car('#')
+        self.extract_car('@')
 
-    def extract_hashtag(self):
-        """Fonction qui extrait les hashtags utilisés dans le tweet à partir d'une base de données et les ajoute à la
-        base de données
+    def extract_car(self, car):
+        """Fonction qui extrait les hashtags utilisés ou les utilisateurs mentionnés dans le tweet à partir d'une base
+        de données et les ajoute à la base de données
         """
 
         txt = self.texte
-        liste_hashtag = self.hashtag
-        fin_hashtag = 0
+        if car == "@":
+            liste_car = self.mention
+            used_car = Tweet.user_mentioned
+            nom_car = 'Mention'
+        elif car == "#":
+            liste_car = self.hashtag
+            used_car = Tweet.used_hashtag
+            nom_car = 'Hashtags'
+        fin_car = 0
         while True:
-            if '#' in txt[fin_hashtag:]:
-                index_hashtag = txt.find('#', fin_hashtag)
-                if index_hashtag != len(txt)-1:
-                    for j in range(index_hashtag+1, len(txt)):
+            if car in txt[fin_car:]:
+                index_car = txt.find(car, fin_car)
+                if index_car != len(txt)-1:
+                    for j in range(index_car+1, len(txt)):
                         if j == len(txt)-1:
                             if txt[j].isalnum():
-                                fin_hashtag = j+1
+                                fin_car = j+1
                             else:
-                                fin_hashtag = j
+                                fin_car = j
                             break
                         elif txt[j] != '_' and not txt[j].isalnum() or txt[j] == '.':
-                            fin_hashtag = j
+                            fin_car = j
                             break
-                    nom_hashtag = txt[index_hashtag:fin_hashtag]
-                    if nom_hashtag != '#':
-                        liste_hashtag.append(nom_hashtag)
-                        if nom_hashtag in Tweet.used_hashtag:
-                            Tweet.used_hashtag[nom_hashtag] += 1
+                    nom_car = txt[index_car:fin_car]
+                    if nom_car != car:
+                        liste_car.append(nom_car)
+                        if nom_car in used_car:
+                            used_car[nom_car] += 1
                         else:
-                            Tweet.used_hashtag[nom_hashtag] = 1
+                            used_car[nom_car] = 1
                     else:
                         break
             else:
@@ -61,6 +72,8 @@ class Tweet:
 
 liste_tweets = [Tweet(data[i]) for i in range(len(data))]
 Tweet.used_hashtag = sorted(Tweet.used_hashtag.items(), key=operator.itemgetter(1), reverse=True)
+Tweet.user_mentioned = sorted(Tweet.user_mentioned.items(), key=operator.itemgetter(1), reverse=True)
 
 print(Tweet.nb_tweets)
 print(Tweet.used_hashtag)
+print(Tweet.user_mentioned)
