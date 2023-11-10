cf442db7eb1ba92e5f5bc4c01554fca57a6c362d
Joseph ARIAS <arias.joseph.biot@gmail.com>
2023-11-10 14:42:25 +0100

    nom de variables en anglais et ajout de la lisibilité des erreurs avec la fonction world_map

--- Projet IN304 class.py
+++ Projet IN304 class.py
@@ -8,6 +8,8 @@ __status__ = "Development"
 
 import json as js
 import time
+
+import geopy.exc
 from textblob import TextBlob
 import random
 import matplotlib.pyplot as plt
@@ -21,6 +23,7 @@ import tkinter as tk
 from tkinter import filedialog
 from PIL import Image, ImageTk
 import emoji
+import os
 
 
 class Tweet:
@@ -34,7 +37,7 @@ class Tweet:
     # neutres et en indice 2 le nombre de tweets positifs
     tweets_objectivity = [0, 0]  # avec en indice 0 le nombre de tweets objectifs et en indice 1 le nombre de tweets
     # subjectifs
-    tweets_localisation = []  # liste de toutes les localisations des tweets
+    tweets_localization = []  # liste de toutes les localisations des tweets
     tweets_time = {str(i).zfill(2): 0 for i in range(24)}  # création des clés dans le dictionnaire dans l'ordre
     # croissant pour améliorer le graphique de la fonction visualize_time
     all_tweets = []  # liste de tous les tweets
@@ -57,7 +60,7 @@ class Tweet:
                 tweet['Polarity'] = ''
                 tweet['Subjectivity'] = ''
                 self.id = tweet['id']
-                self.localisation = tweet['AuthorLocation']
+                self.localization = tweet['AuthorLocation']
                 self.date = tweet['CreatedAt']
                 self.rt = tweet['RetweetCount']
                 self.langue = tweet['TweetLanguage']
@@ -70,24 +73,24 @@ class Tweet:
                 self.author = tweet['Author']
                 self.extract_car('#')  # extraction des hashtags utilisés dans le tweet
                 self.extract_car('@')  # extraction des utilisateurs mentionnés dans le tweet
-                self.analyse_sentiment()  # analyse du sentiment du tweet (négatif, neutre ou positif) et la
+                self.analyze_sentiment()  # analyse du sentiment du tweet (négatif, neutre ou positif) et la
                 # subjectivité
                 self.extract_topics()  # extraction des topics du tweet
                 self.list_tweet_by_author()  # ajout du tweet au dictionnaire tweets_of_users avec en clé l'auteur du
                 # tweet
-                Tweet.used_hashtag_trie = sorted(Tweet.used_hashtag.items(),
+                Tweet.used_hashtag_sorted = sorted(Tweet.used_hashtag.items(),
                                                  key=lambda item: len(item[1]), reverse=True)  # création d'une liste
                 # des hashtags présents dans les tweets analysés triée par ordre décroissant du nombre d'apparitions du
                 # hashtag
-                Tweet.user_mentioned_trie = sorted(Tweet.user_mentioned.items(),
+                Tweet.user_mentioned_sorted = sorted(Tweet.user_mentioned.items(),
                                                    key=lambda item: len(item[1]), reverse=True)  # création d'une liste
                 # des utilisateurs mentionnés dans les tweets analysés triée par ordre décroissant du nombre de mentions
                 # de l'utilisateur
-                Tweet.tweets_of_users_trie = sorted(Tweet.tweets_of_users.items(),
+                Tweet.tweets_of_users_sorted = sorted(Tweet.tweets_of_users.items(),
                                                     key=lambda item: len(item[1]), reverse=True)  # création d'une liste
                 # des utilisateurs triée par ordre décroissant du nombre de tweets de l'utilisateur
-                if self.localisation != "":
-                    Tweet.tweets_localisation.append(self.localisation)
+                if self.localization != "":
+                    Tweet.tweets_localization.append(self.localization)
                 if self.date[11:13] in Tweet.tweets_time:
                     Tweet.tweets_time[self.date[11:13]] += 1
                 else:
@@ -100,13 +103,13 @@ class Tweet:
         """
 
         filepath = filedialog.askopenfilename(title='Ouvrir un fichier json')
-        erreur = 0
+        error = 0
         while not filepath.endswith(".json") and filepath != '':
-            filepath = filedialog.askopenfilename(title=f'OUVRIR UN FICHIER JSON{' !' * erreur}')
-            erreur += 1
+            filepath = filedialog.askopenfilename(title=f'OUVRIR UN FICHIER JSON{' !' * error}')
+            error += 1
 
-        donnees = open(filepath, 'r', encoding='UTF-8')
-        liste_tweets = [js.loads(line) for line in donnees]
+        data = open(filepath, 'r', encoding='UTF-8')
+        list_tweets = [js.loads(line) for line in data]
 
         # Noms d'utilisateurs qui seront ajoutés aux tweets afin de mieux répondre aux questions du projet étant donné
         # qu'aucun nom d'utilisateur n'est fourni (certains noms sont plus susceptibles d'apparaître souvent afin de
@@ -119,30 +122,32 @@ class Tweet:
                     *['@unionetudiante_'] * 99, *['@la_classe_ouvriere'] * 16, *['@ogcnice'] * 6, '@Utah',
                     '@chachat']
 
-        for i in range(len(liste_tweets)):
-            liste_tweets[i]['Author'] = random.choice(users_names)
+        for i in range(len(list_tweets)):
+            list_tweets[i]['Author'] = random.choice(users_names)
 
-        file = Tweet.fill_zone_atterrissage(filepath, liste_tweets)
-        donnees = open(file, 'r', encoding='UTF-8')
-        liste_tweets = [js.loads(line) for line in donnees]
-        for tweet in liste_tweets:
+        file = Tweet.fill_zone_atterrissage(filepath, list_tweets)
+        data = open(file, 'r', encoding='UTF-8')
+        list_tweets = [js.loads(line) for line in data]
+        for tweet in list_tweets:
             Tweet(tweet)
 
-        donnees.close()
+        data.close()
 
-        bouton_file.destroy()
+        button_file.destroy()
         label_file.destroy()
 
-        bouton_analyser = tk.Button(text='Analyser')
-        bouton_analyser.grid()
+        button_analyze = tk.Button(text='Analyser')
+        button_analyze.grid()
 
     @staticmethod
-    def fill_zone_atterrissage(filepath, liste_tweets):
+    def fill_zone_atterrissage(filepath, list_tweets):
         """Remplit la zone d'atterrissage avec tous les objets tweets de la liste de tweets."""
-        new_name = re.match(r'([^\\/]+\.json$)', filepath)
+        new_name = os.path.basename(filepath)
+        new_name = os.path.splitext(new_name)[0]
+
         Tweet.reset_zone_atterrissage(new_name)
         file = open(f'zone_atterrissage_{new_name}.json', 'a')
-        for tweet in liste_tweets:
+        for tweet in list_tweets:
             tweet['TweetText'] = emoji.demojize(tweet['TweetText'])  # on supprime les emojis du tweet
             js.dump(tweet, file)
             file.write('\n')
@@ -166,23 +171,23 @@ class Tweet:
 
         txt = self.texte
         if car == "@":  # si on veut extraire les utilisateurs mentionnés
-            liste_car = self.mention
+            list_car = self.mention
             used_car = Tweet.user_mentioned
-            nom_car = re.findall(r'@\w+', txt)
+            name_car = re.findall(r'@\w+', txt)
         elif car == "#":  # si on veut extraire les hashtags utilisés
-            liste_car = self.hashtag
+            list_car = self.hashtag
             used_car = Tweet.used_hashtag
-            nom_car = re.findall(r'#\w+', txt)
+            name_car = re.findall(r'#\w+', txt)
         else:
             return f'Le caractère {car} ne correspond ni aux hashtags ni aux mentions'
-        for element in nom_car:
-            liste_car.append(element)
+        for element in name_car:
+            list_car.append(element)
             if element in used_car:
                 used_car[element].append(self)
             else:
                 used_car[element] = [self]
 
-    def analyse_sentiment(self):
+    def analyze_sentiment(self):
         """Fonction qui analyse le sentiment d'un tweet (négatif, neutre ou positif)
         """
         sentiment = TextBlob(self.texte).sentiment
@@ -215,46 +220,46 @@ class Tweet:
             Tweet.tweets_of_users[self.author] = [self]
 
 
-def top(liste: list, k: int):
+def top(list_used: list, k: int):
     """Top k hashtags ou Top k utilisateurs mentionnés
 
     Fonction qui affiche les top k hashtags ou les tops k utilisateurs mentionnés
 
     Paramètres
     ----------
-    liste : list
+    list_used : list
         liste des hashtags et leurs occurrences ou liste des mentions d'utilisateurs et leurs occurrences
     k : int
         les k hashtags ou utilisateurs qui reviennent le plus
 
     """
-    nom = []
+    name = []
     occurrence = []
     for i in range(0, k):
-        if liste == Tweet.tweets_of_users_trie:
+        if list_used == Tweet.tweets_of_users_sorted:
             top = 'utilisateur'
             mention = 'tweet'
             xlabel = 'Utilisateurs'
             ylabel = 'Nombre de tweets'
-        elif liste[i][0][0] == '#':
+        elif list_used[i][0][0] == '#':
             top = 'hashtag'
             mention = 'occurrence'
             xlabel = 'Hashtags'
             ylabel = 'Nombre d\'utilisations'
-        elif liste[i][0][0] == '@':
+        elif list_used[i][0][0] == '@':
             top = 'utilisateur mentionné'
             mention = 'mention'
             xlabel = 'Utilisateurs mentionnés'
             ylabel = 'Nombre de mentions'
         else:
-            return f'La liste {liste} n\'est pas compatible avec la fonction top'
+            return f'La liste {list_used} n\'est pas compatible avec la fonction top'
         print(
-            f"Top {i + 1} {top} : {liste[i][0]} avec {len(liste[i][1])} {mention}"
-            f"{'s' if len(liste[i][1]) > 1 else ''}")
-        nom.append(liste[i][0])
-        occurrence.append(len(liste[i][1]))
+            f"Top {i + 1} {top} : {list_used[i][0]} avec {len(list_used[i][1])} {mention}"
+            f"{'s' if len(list_used[i][1]) > 1 else ''}")
+        name.append(list_used[i][0])
+        occurrence.append(len(list_used[i][1]))
 
-    ax = plt.bar(nom, occurrence)
+    ax = plt.bar(name, occurrence)
     for bar in ax:
         height = bar.get_height()
         plt.text(bar.get_x() + bar.get_width() / 2.0, height, f'{height:.0f}', ha='center', va='bottom')
@@ -265,7 +270,7 @@ def top(liste: list, k: int):
     plt.show()
 
 
-def nombre_hashtag(hashtag: str):
+def number_hashtag(hashtag: str):
     """Nombre de publications par hashtag
 
     Fonction qui affiche le nombre de publications pour un hashtag donné
@@ -302,19 +307,19 @@ def publication_author(author: str):
         print('Cet utilisateur n\'a pas tweeté')
 
 
-def show_pie_chart(liste: list):
-    if liste == Tweet.tweets_polarity:
+def show_pie_chart(list_used: list):
+    if list_used == Tweet.tweets_polarity:
         labels = 'Négatif', 'Neutre', 'Positif'
         sizes = Tweet.tweets_polarity
         colors = ['lightcoral', 'silver', 'cornflowerblue']
         title = 'Représentation de la polarité des tweets'
-    elif liste == Tweet.tweets_objectivity:
+    elif list_used == Tweet.tweets_objectivity:
         labels = 'Objectif', 'Subjectif'
         sizes = Tweet.tweets_objectivity
         colors = ['lightcoral', 'cornflowerblue']
         title = 'Représentation de l\'objectivité des tweets'
     else:
-        return f'La liste {liste} n\'est pas compatible avec la fonction show_pie_chart'
+        return f'La liste {list_used} n\'est pas compatible avec la fonction show_pie_chart'
 
     fig, ax = plt.subplots()
     ax.pie(sizes, labels=labels, autopct='%1.1f%%', colors=colors)
@@ -327,19 +332,34 @@ def world_map():
     geolocator = Nominatim(user_agent="Géolocalisation_tweets")
     tweet_coordinates = {}
     i = 1
-    for location in Tweet.tweets_localisation:
+    for location in Tweet.tweets_localization:
         try:
             location = location.strip()
             if location not in tweet_coordinates:
                 location_data = geolocator.geocode(location)
                 if location_data:
                     tweet_coordinates[location] = (location_data.latitude, location_data.longitude, location)
-        except Exception:
-            pass
-        print(i, '/', len(Tweet.tweets_localisation))
-        i += 1
+            if i % 10 == 0 or i == 1:
+                print(i, '/', len(Tweet.tweets_localization))
+            i += 1
+        except ConnectionError:
+            print(f'Une erreur de connection est survenue lors de la création de la carte avec la localisation '
+                  f'suivante: {location}')
+            i += 1
+        except TimeoutError:
+            print(f'Une erreur de type Timeout est survenue lors de la création de la carte avec la localisation '
+                  f'suivante: {location}')
+            i += 1
+        except geopy.exc.GeocoderUnavailable:
+            print(f'Une erreur est survenue lors de la création de la carte avec la localisation '
+                  f'suivante: {location}. Soit la connexion n\'a pas pu être établis avec le service de géolocalisation'
+                  f' soit le service n\'est pas disponible')
+        except Exception as error:
+            print(f'Une erreur de type {type(error)}est survenue lors de la création de la carte avec la localisation '
+                  f'suivante: {location}')
+            i += 1
 
-    tweet_counts = Counter(Tweet.tweets_localisation)
+    tweet_counts = Counter(Tweet.tweets_localization)
     m = folium.Map()
 
     for location, count in tweet_counts.items():
@@ -389,20 +409,20 @@ render = ImageTk.PhotoImage(load)
 window.iconphoto(False, render)
 label_file = tk.Label(window, text='Choisir un fichier contenant des tweets au format json', font=('helvetica', '20'),
                       fg='#00acee')
-bouton_file = tk.Button(window, text='Choisir le fichier', command=Tweet.instantiate_from_file)
+button_file = tk.Button(window, text='Choisir le fichier', command=Tweet.instantiate_from_file)
 label_file.grid()
-bouton_file.grid()
+button_file.grid()
 window.mainloop()
 
 print(f"Nombre de tweets analysés: {len(Tweet.all_tweets) + 1}")
-print(top(Tweet.user_mentioned_trie, 15))
-print(top(Tweet.used_hashtag_trie, 15))
-print(top(Tweet.tweets_of_users_trie, 15))
-print(nombre_hashtag("#AI"))
+print(top(Tweet.user_mentioned_sorted, 15))
+print(top(Tweet.used_hashtag_sorted, 15))
+print(top(Tweet.tweets_of_users_sorted, 15))
+print(number_hashtag("#AI"))
 print(publication_author('Chumlee'))
 show_pie_chart(Tweet.tweets_polarity)
 show_pie_chart(Tweet.tweets_objectivity)
-"""world_map() # cette fonction demande beaucoup de temps pour s'exécuter et dépend de la connexion internet ! La
+world_map() # cette fonction demande beaucoup de temps pour s'exécuter et dépend de la connexion internet ! La
 # console affiche l'avancement de cette dernière"""
 
 visualize_tweet_time()
